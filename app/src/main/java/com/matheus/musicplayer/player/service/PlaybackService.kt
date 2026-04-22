f14bacaf366c183627297bda1d3b1c7566af5321
Matheus Esteves <matheus.esteves@papayapay.com>
2026-04-22 20:15:51 -0300

    Customizing notification

--- app/src/main/java/com/matheus/musicplayer/player/service/PlaybackService.kt
+++ app/src/main/java/com/matheus/musicplayer/player/service/PlaybackService.kt
@@ -1,5 +1,6 @@
 package com.matheus.musicplayer.player.service
 
+import android.content.Intent
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
@@ -12,6 +13,12 @@ class PlaybackService : MediaSessionService() {
         super.onCreate()
         val player = ExoPlayer.Builder(this).build()
         mediaSession = MediaSession.Builder(this, player).build()
+    }
+
+    // This is to stop the service if the user kills the app
+    override fun onTaskRemoved(rootIntent: Intent?) {
+        mediaSession?.player?.stop()
+        stopSelf()
     }
 
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? =
